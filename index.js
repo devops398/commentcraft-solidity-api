4415b092b3ddcb6cb7a346b0bd9f4185be114416
Igor Limansky <i.e.limansky@gmail.com>
2017-07-06 20:30:26 +0300

    fixed getUsername, added getQtumAddress, getHexAddress methods

--- index.js
+++ index.js
@@ -9,38 +9,39 @@ const RPC_USER     = 'test';
 const RPC_PASSWORD = 'test';
 const provider     = new Web4.providers.HttpProvider(`http://${RPC_USER}:${RPC_PASSWORD}@${RPC_HOST}:${RPC_PORT}`);
 window.web4        = new Web4(provider);
-//const account = window.web4.eth.accounts[0];
-const commentcraftContract = window.web4.qtum.contract(require('./build/contracts/CommentCraft.json')).at(CONTRACT_ADR);
-/* Commentcraft.deployed()
- .then(instance => {
- window.CCONTRACT = instance;
- });*/
 
-const getAbiData = (methodName, data) => commentcraftContract[methodName].getData(data);
+const abi = require('./build/contracts/CommentCraft.json').abi;
+const commentcraftContract = window.web4.qtum.contract(abi).at(CONTRACT_ADR);
+
+const getAbiData = (methodName, data) => commentcraftContract[methodName].getData(data).substring(2);
 
 const getId = ( data ) => parseInt(XXH.h32(data, 0xABCD).toString(16), 16);
 
-const getPosts = id => Commentcraft.deployed()
-.then(instance => instance.getPagespostsData.call(id));
+const getQtumAddress = (account="") => web4.qtum.getAccountAddress(account);
+const getHexAddress = (qtumAddress) => web4.qtum.getHexAddress(qtumAddress);
 
-const getPost = id => Commentcraft.deployed()
-.then(instance => instance.getPost.call(id));
+/*
 
-const getUsername = id => web4.qtum.callContract(CONTRACT_ADR, getAbiData('getUsername', id))
+ const getPosts = id => Commentcraft.deployed()
+ .then(instance => instance.getPagespostsData.call(id));
 
-const createPost = ( pageId, content ) => Commentcraft.deployed()
-.then(instance => {
-  const postId = getId(`${pageId}${content}${Math.random()}`); // uniq post id
+ const getPost = id => Commentcraft.deployed()
+ .then(instance => instance.getPost.call(id));
+ */
 
-  return instance.createPost(pageId, postId, content, { from: ""/*account */ })
-  .then(() => postId);
-});
+const getUsername = id => web4.toAscii(qtum.callContract(CONTRACT_ADR.substring(2), getAbiData('getUsername', id)).executionResult.output)
+/*
+
+ const createPost = ( pageId, content ) => Commentcraft.deployed()
+ .then(instance => {
+ const postId = getId(`${pageId}${content}${Math.random()}`); // uniq post id
+
+ return instance.createPost(pageId, postId, content, { from: ""/!*account *!/ })
+ .then(() => postId);
+ });
+ */
 
 export default {
   commentcraftContract,
-  getId,
-  getPost,
-  getPosts,
-  getUsername,
-  createPost
+  getUsername
 }
